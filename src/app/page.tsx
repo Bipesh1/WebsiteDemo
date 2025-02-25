c6867e851824bd39660f4fc8829c1ccfa4766f62
Bipesh <adbipesh001@gmail.com>
2025-02-25 13:57:03 +0545

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import BestSeller from "@/components/sections/best-seller";
 import HeroSection from "@/components/sections/hero-section";
-import { div } from "framer-motion/client";
-import Image from "next/image";
 
 export default function Home() {
   return (
