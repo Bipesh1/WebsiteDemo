3b3062acb9f5dcbff3bac0af0ddc9551c466fd65
Bipesh <adbipesh001@gmail.com>
2025-02-25 19:11:13 +0545

    merged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import BestSeller from "@/components/sections/best-seller";
 import HeroSection from "@/components/sections/hero-section";
+import HotDealsSection from "@/components/sections/hot-deals-section";
+import FooterSection from "@/components/sections/footer";
 import OurServices from "@/components/sections/our-services";
 
 export default function Home() {
