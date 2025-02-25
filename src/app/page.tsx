d5a0998e144b28d0bdb34e0e3e2c369a4debda45
Bipesh <adbipesh001@gmail.com>
2025-02-25 16:47:59 +0545

    added pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import BestSeller from "@/components/sections/best-seller";
 import HeroSection from "@/components/sections/hero-section";
 import FooterSection from "@/components/sections/footer";
+import HotDealsSection from "@/components/sections/hot-deals-section";
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <HeroSection/>
       <BestSeller/>
+      <HotDealsSection/>
       <FooterSection/>
     </div>
   );
